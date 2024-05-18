bc94ab58a477d27c70bec9d6960abe3e638983be
dev <166125538+Purohitdev@users.noreply.github.com>
2024-05-18 14:30:39 +0530

    Update Contact.jsx

--- src/componet/Home/Contact.jsx
+++ src/componet/Home/Contact.jsx
@@ -16,20 +16,25 @@ function Contact() {
                         <h5>Feel free to reach out to me. I'm always open to discuss new projects </h5>
                         <div className='stay'>
                             <h6>STAY UP TO DATE</h6>
-                            <p>(+053) 625 08 09 62</p>
-                            <p>kevincontact@gmail.com</p>
+                            <p>+91-9653364287</p>
+                            <p>devanshrajpurohit1@gmail.com</p>
                         </div>
                         <div className='stay'>
                             <h6>ADDRESS</h6>
-                            <p className='pp'>223 Thatcher Road St, Brooklyn, Manhattan, NY 10463</p>
+                            {/* <p className='pp'>223 Thatcher Road St, Brooklyn, Manhattan, NY 10463</p> */}
                             <div className="link-b">
-                                <div className="icon-con rightt">Facebook <ImArrowUpRight2 className='some' /></div>
+                                <a href="https://www.facebook.com/jitesh.rajpurohit.140">
+                                    <div className="icon-con rightt">Facebook <ImArrowUpRight2 className='some' /></div>
+                                </a>
+                                <a href="https://www.linkedin.com/in/devansh-rajpurohit/">
                                 <div className="icon-con">Linkedin <ImArrowUpRight2 className='some' /></div>
+
+                                </a>
                             </div>
-                            <div className="link-b">
+                            {/* <div className="link-b">
                                 <div className="icon-con rightt">Behance <ImArrowUpRight2 className='some' /></div>
                                 <div className="icon-con">Dribbble <ImArrowUpRight2 className='some' /></div>
-                            </div>
+                            </div> */}
 
 
                         </div>
@@ -68,17 +73,17 @@ function Contact() {
                                     }}
                                     onSubmit={(values, { setSubmitting }) => {
                                         setTimeout(() => {
-                                            const userdata={
-                                                Name:values.Name,
-                                                Email:values.Email,
-                                                Subject:values.Subject,
-                                                Phone:values.Phone,
-                                                Message:values.Message,
-                                                Pincode:values.Pincode,
+                                            const userdata = {
+                                                Name: values.Name,
+                                                Email: values.Email,
+                                                Subject: values.Subject,
+                                                Phone: values.Phone,
+                                                Message: values.Message,
+                                                Pincode: values.Pincode,
 
 
-                                             };
-                                             console.log(userdata)
+                                            };
+                                            console.log(userdata)
                                         }, 400);
                                     }}
                                 >
@@ -162,29 +167,29 @@ function Contact() {
                                                 {errors.Message && touched.Message && errors.Message}</p>
 
 
-                                        <div className="input-last">
+                                            <div className="input-last">
 
-                                        <input
-                                                placeholder='Pincode'
-                                                type="Pincode"
-                                                name="Pincode"
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                value={values.Pincode}
-                                            />
-                                            <p className='error'> ~
-                                                {errors.Pincode && touched.Pincode && errors.Pincode}</p>
+                                                <input
+                                                    placeholder='Pincode'
+                                                    type="Pincode"
+                                                    name="Pincode"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.Pincode}
+                                                />
+                                                <p className='error'> ~
+                                                    {errors.Pincode && touched.Pincode && errors.Pincode}</p>
 
-                                        </div>
+                                            </div>
 
 
 
-                                         <div className="butt">
-                                         <button type="submit" disabled={isSubmitting}>
-                                                Submit <span className='arrowww'>
-                                               <BiRightArrow className='arroww' /></span>
-                                            </button>
-                                         </div>
+                                            <div className="butt">
+                                                <button type="submit" disabled={isSubmitting}>
+                                                    Submit <span className='arrowww'>
+                                                        <BiRightArrow className='arroww' /></span>
+                                                </button>
+                                            </div>
                                         </form>
                                     )}
                                 </Formik>
