8dac3387213e458cf7ebf3a593fa130c594f8248
dev <166125538+Purohitdev@users.noreply.github.com>
2024-10-09 20:14:58 +0530

    zoneis update

--- src/componet/Home/Port.jsx
+++ src/componet/Home/Port.jsx
@@ -35,7 +35,7 @@ function Port() {
             detail: "management",
             a: "https://crudd-login.vercel.app/",
         },
-           {
+        {
             id: 3,
             img: `${bol}`,
             title: "React (Drag animation)",
@@ -85,20 +85,21 @@ function Port() {
             detail: "Evie portfolio",
             a: "https://eviee.vercel.app/",
         },
-          {
-            id:10,
-            img:`${gum}`,
+        {
+            id: 10,
+            img: `${zon}`,
+            title: "React + backend ",
+            detail: "zoneis",
+            a: "https://zoneis.vercel.app/"
+        }
+        , {
+            id: 11,
+            img: `${gum}`,
             title: "React (GSAP )",
             detail: "Gumshoes {in progress..}",
-            a:"https://gumshoe-zeta.vercel.app/"
+            a: "https://gumshoe-zeta.vercel.app/"
         },
-        {
-            id:11,
-            img:`${zon}`,
-            title: "React + backend ",
-            detail: "zoneis {in progress..}",
-            a:"https://zoneis.vercel.app/"
-        }
+
 
     ]
     return (
@@ -128,25 +129,25 @@ function Port() {
                             potdata.map((elem, index) => {
                                 return (
                                     <Col lg={6}>
-                                       <div className='card-pot'> 
-                                       <a href={elem.a} target='_blank'>
-                                            <div className="box-pot">
-                                                <div className="img-boc">
-                                                    <img src={elem.img} alt="" />
-
-                                                </div>
-                                                <h6> {elem.id} &nbsp; // &nbsp; {elem.title}</h6>
-                                                <div className="h1-down">
-                                                    <h1> {elem.detail}    </h1>
-                                                    <div className="icon-box-pot">
-                                                        <GoArrowUpRight />
+                                        <div className='card-pot'>
+                                            <a href={elem.a} target='_blank'>
+                                                <div className="box-pot">
+                                                    <div className="img-boc">
+                                                        <img src={elem.img} alt="" />
 
                                                     </div>
+                                                    <h6> {elem.id} &nbsp; // &nbsp; {elem.title}</h6>
+                                                    <div className="h1-down">
+                                                        <h1> {elem.detail}    </h1>
+                                                        <div className="icon-box-pot">
+                                                            <GoArrowUpRight />
 
+                                                        </div>
+
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </a>
-                                       </div>
+                                            </a>
+                                        </div>
                                     </Col>
                                 )
 
